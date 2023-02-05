29909c4d5abb6ff4d5c28fd67e15bde24e97ea9b
grpht <gr6phite@gmail.com>
2023-02-05 23:13:36 +0900

    Troubleshooting Incorrect Header Size

--- Source/TCPClientPlugin/Private/TCPPacketHeader.h
+++ Source/TCPClientPlugin/Private/TCPPacketHeader.h
@@ -5,9 +5,11 @@
 #include "CoreMinimal.h"
 
 //change member as you want, But don't use virtual 
+#pragma pack(push, 1)
 struct TCPPacketHeader
 {
 	int32 Size; //*Don't Rename, but allow changing byte size 
 	//int16 Any; //add or remove any header
 	int16 Id;
 };
+#pragma pack(pop)
